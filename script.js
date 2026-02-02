e1768a3258330ac08b8d9e43e6206bc229c37c67
princeki20 <kiruhup23@bonaventure.edu>
2026-02-01 20:10:04 -0500

    second

---
+++ script.js
@@ -0,0 +1,10 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 100);
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
